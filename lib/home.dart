f2e3967dd32f0ddf0e2bc27304273c6e546b98a9
Abdullah Masood <amasood@octans.ai>
2021-06-28 00:45:49 +0500

    Added the form validation and pass object

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+class UserData {
+  String? fname;
+  String? lname;
+  String? email;
+  String? password;
+  UserData({this.fname, this.lname, this.email, this.password});
+}
+
 class SignUpForm extends StatefulWidget {
   SignUpForm({Key? key}) : super(key: key);
 
@@ -10,9 +18,26 @@ class SignUpForm extends StatefulWidget {
 class _SignUpFormState extends State<SignUpForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  validated() {
+
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  validated(BuildContext context) {
+  
+
+      UserData user = new UserData(
+        fname: firstNameController.text,
+        lname: lastNameController.text,
+        email: emailController.text,
+        password: passwordController.text);
+
+
     if (_formKey.currentState != null && _formKey.currentState!.validate()) {
       print("Form Validated");
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => HomeScreen(user: user)));
     } else {
       print("Form Not Validated");
       return;
@@ -43,6 +68,7 @@ class _SignUpFormState extends State<SignUpForm> {
                   child: TextFormField(
                     decoration:
                         InputDecoration(labelText: 'Enter the first Name'),
+                        controller: firstNameController,
                     validator: (String? value) {
                       if (value == null || value.trim().length == 0) {
                         return "Required";
@@ -56,6 +82,7 @@ class _SignUpFormState extends State<SignUpForm> {
                   child: TextFormField(
                     decoration:
                         InputDecoration(labelText: 'Enter the Last Name'),
+                        controller: lastNameController,
                     validator: (String? value) {
                       if (value == null || value.trim().length == 0) {
                         return "Required";
@@ -68,6 +95,7 @@ class _SignUpFormState extends State<SignUpForm> {
                   padding: EdgeInsets.all(8.0),
                   child: TextFormField(
                     decoration: InputDecoration(labelText: 'Enter the Email'),
+                    controller: emailController,
                     validator: (String? value) {
                       if (value == null && value!.trim().length == 0) {
                         return "Field is required";
@@ -86,6 +114,7 @@ class _SignUpFormState extends State<SignUpForm> {
                   child: TextFormField(
                     decoration:
                         InputDecoration(labelText: 'Enter the Password'),
+                        controller: passwordController,
                     obscureText: true,
                     validator: (String? value) {
                       if (value == null && value!.trim().length == 0) {
@@ -103,12 +132,60 @@ class _SignUpFormState extends State<SignUpForm> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    validated();
+                    validated(context);
                   },
                   child: Text('Sign Up'),
                 )
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+class HomeScreen extends StatelessWidget {
+  final UserData user;
+  HomeScreen({Key? key, required this.user}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Text("First Name: ${user.fname}"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Text("Last Name: ${user.lname}"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Text("Email: ${user.email}"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Text("Password: ${user.password}"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Go Back"))
+            ],
           ),
         ),
       ),
